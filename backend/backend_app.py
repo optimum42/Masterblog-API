e7c47a3544d0222565b11ea2a8bfe5c353db367d
optimum42 <holgermassek@gmail.com>
2026-06-01 17:05:40 +0200

    sorting feature added

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,13 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-
-def get_next_id():
-    if len(POSTS) == 0:
-        return 1
-    return max(post["id"] for post in POSTS) + 1
-
-
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
@@ -17,13 +10,44 @@ POSTS = [
 ]
 
 
+def get_next_id():
+    """ Returns the next available post id """
+    if len(POSTS) == 0:
+        return 1
+    return max(post["id"] for post in POSTS) + 1
+
+
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
-    return jsonify(POSTS)
+    """
+    Returns a list of all posts
+    optional query params can control the sorting:
+    sort=title: sorted by title
+    sort=content: sorted by content
+    direction=asc sort in ascending order
+    direction=desc sort in descending order
+    """
+    sort = request.args.get('sort', '').strip().lower()
+    direction = request.args.get('direction', '').strip().lower()
+    if sort == "":
+        return jsonify(POSTS), 200
+
+    if sort not in ["title", "content"]:
+        message = "Please provide 'title' or 'content' as a sorting parameter."
+        return jsonify({"message": message}), 400
+
+    if direction != "" and direction not in ["asc", "desc"]:
+        message = "Please provide 'asc' or 'desc' as a direction parameter."
+        return jsonify({"message": message}), 400
+
+    sorted_posts = sorted(POSTS, key=lambda x: x[sort],
+                               reverse=direction == "desc")
+    return jsonify(sorted_posts), 200
 
 
 @app.route('/api/posts', methods=['POST'])
 def add_post():
+    """ Adds a new post to the list of posts """
     data = request.get_json()
     title = data.get("title").strip()
     content = data.get("content").strip()
@@ -40,6 +64,10 @@ def add_post():
 
 @app.route('/api/posts/<int:id>', methods=['DELETE'])
 def delete_post(id):
+    """
+    Deletes a post from the list of posts and returns a success message
+    If post id is not found, returns a 404 status and a message
+    """
     for post in POSTS:
         if post["id"] == id:
             POSTS.remove(post)
@@ -50,6 +78,11 @@ def delete_post(id):
 
 @app.route('/api/posts/<int:id>', methods=['PUT'])
 def update_post(id):
+    """
+    Updates a post from the list of posts
+    Returns the updated post if post id exists
+    If post id is not found, returns a 404 status and a message
+    """
     post_exists = False
     for post in POSTS:
         if post["id"] == id:
@@ -68,6 +101,10 @@ def update_post(id):
 
 @app.route("/api/search", methods=["GET"])
 def search_posts():
+    """
+    Returns a filtered list of all posts where query params
+    title and/or content matches the appropriate fields in the posts
+    """
     title = request.args.get('title', '').strip().lower()
     content = request.args.get('content', '').strip().lower()
     filtered_posts = []
