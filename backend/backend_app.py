d5447f43b130c137dabd12205d2df76ba779f5d8
optimum42 <holgermassek@gmail.com>
2026-06-01 16:22:51 +0200

    search_post() added

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+
+def get_next_id():
+    if len(POSTS) == 0:
+        return 1
+    return max(post["id"] for post in POSTS) + 1
+
+
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
@@ -18,26 +25,21 @@ def get_posts():
 @app.route('/api/posts', methods=['POST'])
 def add_post():
     data = request.get_json()
-    title = data.get("title")
-    content = data.get("content")
+    title = data.get("title").strip()
+    content = data.get("content").strip()
 
     if title is None or title == "":
-        return jsonify({"message": "Please provide a title."}), 401
+        return jsonify({"message": "Please provide a title."}), 400
     if content is None or content == "":
-        return jsonify({"message": "Please provide a content."}), 401
+        return jsonify({"message": "Please provide a content."}), 400
 
-    post = {"id": 3, "title": title, "content": content}
+    post = {"id": get_next_id(), "title": title, "content": content}
     POSTS.append(post)
     return jsonify(post), 201
 
 
-@app.route('/api/posts/<id>', methods=['DELETE'])
+@app.route('/api/posts/<int:id>', methods=['DELETE'])
 def delete_post(id):
-    try:
-        id = int(id)
-    except ValueError:
-        return jsonify({"message": "Please provide an integer id."}), 401
-
     for post in POSTS:
         if post["id"] == id:
             POSTS.remove(post)
@@ -46,13 +48,8 @@ def delete_post(id):
     return jsonify({"message": f"Post with id={id} not found."}), 404
 
 
-@app.route('/api/posts/<id>', methods=['PUT'])
+@app.route('/api/posts/<int:id>', methods=['PUT'])
 def update_post(id):
-    try:
-        id = int(id)
-    except ValueError:
-        return jsonify({"message": "Please provide an integer id."}), 401
-
     post_exists = False
     for post in POSTS:
         if post["id"] == id:
@@ -67,6 +64,23 @@ def update_post(id):
     post["content"] = data.get("content") or post.get("content")
 
     return jsonify(post), 200
+
+
+@app.route("/api/search", methods=["GET"])
+def search_posts():
+    title = request.args.get('title', '').strip().lower()
+    content = request.args.get('content', '').strip().lower()
+    filtered_posts = []
+    for post in POSTS:
+        if title != "" and title in post["title"].lower():
+            filtered_posts.append(post)
+
+        if (content != ""
+                and content in post["content"].lower()
+                and post not in filtered_posts): # don't add double
+            filtered_posts.append(post)
+
+    return jsonify(filtered_posts), 200
 
 
 if __name__ == '__main__':
